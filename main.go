91a70cebb1229cbbfab0bdfa8ca701c8bcb28362
Joe Clark <clarkjoe.co@gmail.com>
2022-02-19 09:37:02 -0700

    add dir structure; add basic table generation

--- main.go
+++ main.go
@@ -1,56 +1,20 @@
 package main
 
 import (
-    "fmt"
     "html/template"
     "log"
     "net/http"
-    "time"
 )
 
-type Player struct {
-    ID string `json:"id"`
-    Username string `json:"username"`
-    Catergories []string `json:"catergories"`
-}
-
-type Game struct {
-    ID string `json:"id"`
-    Players []Player `json:"players"`
-    TOC time.Time `json:"toc"`
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("home.html")
+    t, _ := template.ParseFiles("tmpl/index.html")
     t.Execute(w, nil)
-}
-
-func createGameHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "createGameHandler. %s", r.URL.Path[1:])
-}
-
-func joinGameHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "joinGameHandler. %s", r.URL.Path[1:])
-}
-
-func startGameHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "startGameHandler. %s", r.URL.Path[1:])
-}
-
-func addCategoryHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "addCategoryHandler. %s", r.URL.Path[1:])
-}
-
-func displayListHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "displayListHandler. %s", r.URL.Path[1:])
 }
 
 func main() {
     http.HandleFunc("/clarkegories", homeHandler)
-    http.HandleFunc("/clarkegories/creategame", createGameHandler)
-    http.HandleFunc("/clarkegories/joingame", joinGameHandler)
-    http.HandleFunc("/clarkegories/startgame", startGameHandler)
-    http.HandleFunc("/clarkegories/addcategory", addCategoryHandler)
-    http.HandleFunc("/clarkegories/displaylist", displayListHandler)
+
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ static/js/fullList.js
@@ -0,0 +1,30 @@
+const tableHeaders = [
+    "Category"
+]
+const catergories = [
+    "cateory1",
+    "cateory2"
+]
+
+function generateTableHead(table) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let tableHeader of tableHeaders) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(tableHeader);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTableRows(table) {
+    for (let category of catergories) {
+        let row = table.insertRow();
+        let text = document.createTextNode(category)
+        row.appendChild(text)
+    }
+}
+
+let table = document.querySelector("table");
+generateTableHead(table);
+generateTableRows(table);
